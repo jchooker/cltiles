38d8fa5ba68866c45b755ead5f351a9d33ceecdb
Joseph Hooker <jcolehooker@gmail.com>
2023-11-28 09:45:51 -0600

    cube appears, and is fully visible, even when rotated; now we need to change the color, add an outline to every edge, have the image appear correctly on top of it, flatten, and so on

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import * as THREE from 'three';
 // import { BrowserRouter, Routes } from 'react-router-dom';
 import { Canvas, useLoader } from '@react-three/fiber';
+import {OrbitControls} from '@react-three/drei';
 import {Tile} from './components/Tile';
 
 function App() {
   const imgUrl = "https://i.ibb.co/vV6CJM4/asmodeus.png";
   return (
-      <Canvas>
-        <ambientLight />
-        {/* <Tile imgUrl={imgUrl} size={2} radius={0.1}></Tile> */}
-        <Tile imgUrl={imgUrl}></Tile>
-      </Canvas>
-  )
+    <Canvas camera={{position: [0, 0, 10], fov: 50, near: 0.1, far: 1000}}>
+      {/* <ambientLight /> */}
+      {/* <Tile imgUrl={imgUrl} size={2} radius={0.1}></Tile> */}
+      <OrbitControls></OrbitControls>
+      <Tile imgUrl={imgUrl}></Tile>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} />
+    </Canvas>
+  );
 }
 
 export default App
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,58 +1,36 @@
 import React, { useEffect, useRef, useState } from 'react';
 // import { useFrame } from '@react-three/fiber';
 // import { extend } from '@react-three/fiber'
-import * as THREE from 'three';
-import { TextureLoader } from 'three';
+import {MeshStandardMaterial,Mesh,BoxGeometry,MeshBasicMaterial,TextureLoader} from "three";
+// import { TextureLoader } from 'three';
 // import { DragControls } from 'three/addons/controls/DragControls.js';
-import {OrbitControls} from '@react-three/drei';
+// import {OrbitControls} from '@react-three/drei';
 import { useLoader } from "@react-three/fiber";
 
-export const Tile = ({imgUrl, size=10, radius=1}) => {
+export const Tile = ({imgUrl, size=5, radius=1}) => {
     const [texture, setTexture] = useState(null);
-    const mesh = useRef();
+    const cubeRef = useRef();
     const [position, setPosition] = useState([0, 0, 0]);
-    const srfcOverlay = useLoader(THREE.TextureLoader, imgUrl);
-    //loading img texture
+    // const srfcOverlay = useLoader(THREE.TextureLoader, imgUrl);
+    const cubeGeometry = new BoxGeometry(size, size, size);
+    const cubeMaterial = new MeshStandardMaterial({color: 0x00ff00});
+    const cubeMesh = new Mesh(cubeGeometry, cubeMaterial);
+
     useEffect(() => {
-        const loadTexture = () => {
-            const loader = new TextureLoader();
-            loader.load(imgUrl, (loadedTexture) => {
-                if (mesh.current) {
-                    setTexture(loadedTexture);
-                }
-            });
-        };
+        const textureLoader = new TextureLoader();
+        setTexture(textureLoader.load(imgUrl));
+    }, [imgUrl]);
 
-        //Trigger texture load
-        if (imgUrl && !texture) {
-            loadTexture();
-        }
+    const cubeTopMaterial = new MeshBasicMaterial({map: texture});
 
-    //"Clean up" texture on unmount
-        return () => {
-            if (texture) {
-                texture.dispose();
-            }
-        };
-    }, [imgUrl, texture]);
-    return (
-        <>
-            <OrbitControls />
-            <mesh ref={mesh} position={position}>
-                <boxGeometry attach="geometry" args={[size, size, Math.floor(size/2)]} />
-                <meshBasicMaterial attach="material" map={srfcOverlay} />
-            </mesh>
-        </>
-    );
-}
-
-// extend({DragControls});
-
-// const Tile = ({imgUrl, size = 1, radius = 0.1}) => {
-//     const [texture, setTexture] = useState(null);
-//     const mesh = useRef();
-//     const [position, setPosition] = useState([0, 0, 0]);
-
+    cubeMesh.material = [
+        new MeshStandardMaterial({ color: 0x00ff00 }), // Material for other faces
+        new MeshStandardMaterial({ color: 0x00ff00 }), // Material for other faces
+        cubeTopMaterial, // Material for the face where you want to overlay the image
+        new MeshStandardMaterial({ color: 0x00ff00 }), // Material for other faces
+        new MeshStandardMaterial({ color: 0x00ff00 }), // Material for other faces
+        new MeshStandardMaterial({ color: 0x00ff00 }),
+    ];
     //loading img texture
     // useEffect(() => {
     //     const loadTexture = () => {
@@ -64,44 +42,47 @@ export const Tile = ({imgUrl, size=10, radius=1}) => {
     //         });
     //     };
 
-        //Trigger texture load
+    //Trigger texture load
         // if (imgUrl && !texture) {
         //     loadTexture();
         // }
 
     //"Clean up" texture on unmount
-    //     return () => {
-    //         if (texture) {
-    //             texture.dispose();
-    //         }
-    //     };
-    // }, [imgUrl, texture]);
-
-    //Animate tile
-    // useFrame(() => {
-    //     if (mesh.current) {
-
+    // return () => {
+    //     if (texture) {
+    //         texture.dispose();
     //     }
-    // });
+    // };
+    // }, [imgUrl, texture]);
+    // const loadTexture = () => {
+    //     const loader = new TextureLoader();
+    //     loader.load(imgUrl, (loadedTexture) => {
+    //         if (mesh.current) {
+    //             setTexture(loadedTexture);
+    //         }
+    //     });
+    // };
 
-//     const handleDrag = (event) => {
-//         const [x, y, z] = event.object.position.toArray();
-//         setPosition([x, y, z]);
-//     }
+  //Trigger texture load
+    // if (imgUrl && !texture) {
+    //     loadTexture();
+    // }
 
-//     return (
-//         <mesh ref={mesh} position={position}>
-//         <boxBufferGeometry attach="geometry" args={[size, size, size]} />
-//         <meshStandardMaterial attach="material" map={texture} />
-//         <roundedBoxBufferGeometry
-//             args={[size, size, size, radius, 0, radius, 0, 0]}
-//         />
-//         <dragControls
-//             args={[mesh.current]}
-//             onDrag={handleDrag}
-//         />
-//         </mesh>
-//     );
-// };
-
-// export default Tile;
+  //"Clean up" texture on unmount
+  // return () => {
+  //     if (texture) {
+  //         texture.dispose();
+  //     }
+  // };
+    return (
+        <>
+            <mesh ref={cubeRef} position={position}>
+                <boxGeometry
+                    attach="geometry"
+                    args={[size, size, size]}
+                />
+                <meshStandardMaterial color={0x00ff00} />
+            </mesh>
+        </>
+    );
+}
